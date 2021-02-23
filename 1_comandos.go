68cb1cb53d4f9d1f6a9ae83f84e2a20b5d174a81
Josué Hormeño <76787941+Jotahm@users.noreply.github.com>
2021-02-23 18:23:09 +0100

    Create 1_comandos.go

---
+++ 1_comandos.go
@@ -0,0 +1,15 @@
+#go run permite compilar el archivo que seleccionemosgo
+#go doc + funcion nos da informacion ; por ejemplo go doc print
+#go fmt ; repara el formato (p.e. indentacion; permite que sea facil de leer, legible)
+#go build (compila la app sin ejecutarla)
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Hola mundo")
+
+}
+	
